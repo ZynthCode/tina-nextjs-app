0679b7dfa4b91703a97f1403a5b0e65cd16b2bb6
Zynth <146667056+ZynthCode@users.noreply.github.com>
2024-05-01 13:25:59 +0200

    feat: implemented tinacms backend  (#1)
    feat: implemented tinacms backend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,33 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "var(--color-primary)",
+        "primary-light": "var(--color-primary-light)",
+        "primary-dark": "var(--color-primary-dark)",
+        "primary-contrast": "var(--color-primary-contrast)",
+
+        secondary: "var(--color-secondary)",
+        "secondary-light": "var(--color-secondary-light)",
+        "secondary-dark": "var(--color-secondary-dark)",
+        "secondary-contrast": "var(--color-secondary-contrast)",
+
+        "background-primary": "var(--color-background-primary)",
+        "background-secondary": "var(--color-background-secondary)",
+
+        "text-primary": "var(--color-text-primary)",
+        "text-secondary": "var(--color-text-secondary)",
+        "text-link": "var(--color-text-link)",
+
+        "footer-text": "var(--color-footer-text)",
+
+        warning: "var(--color-warning)",
+        "warning-light": "var(--color-warning-light)",
+        "warning-dark": "var(--color-warning-dark)",
+        "warning-contrast": "var(--color-warning-contrast)",
       },
     },
   },
